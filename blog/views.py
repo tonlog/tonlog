294809d1c1221d4b2649717ee8a62e655c69e943
unknown <njusoftware@njusoftware-PC.(none)>
2012-09-16 16:59:11 +0800

    app

--- blog/views.py
+++ blog/views.py
@@ -38,7 +38,7 @@ def contactUs(request):
                 sender,
                 [DEFAULT_MAIL_RECEIVER],
             )
-            return HttpResponseRedirect("/contact/thx")
+            return HttpResponseRedirect("/contactUs/thx")
     else:
         form = ContactForm()
     return render_to_response("contactUs.html",{'form':form},context_instance=RequestContext(request))
