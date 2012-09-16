4b86591405a5f4791103325cdf284bfdc4d86261
unknown <njusoftware@njusoftware-PC.(none)>
2012-09-16 16:39:23 +0800

    app

--- newApp/settings.py
+++ newApp/settings.py
@@ -161,4 +161,4 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'tonie.tonieh.h@gmail.com'
 EMAIL_HOST_PASSWORD = 'kakashi1109'
-EMAIL_PORT = 587
+EMAIL_PORT = 25
